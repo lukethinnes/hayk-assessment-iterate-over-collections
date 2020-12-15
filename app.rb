b51620e78f60cbfea25dc7869af3fe46d5a20545
Luke Thinnes <lukethinnes@yahoo.com>
2020-12-15 11:45:49 -0700

    commit tree 090937ba73bb69089073f0b3e20be0c0ccf53563

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -14,3 +16,29 @@ companies = [
 ]
 
 ### Your code below this
+def find_name_and_size
+
+end
+
+def combine_all_sizes
+
+end
+
+def companies_over_100
+
+end
+
+def find_beta
+    Company.find do |name|
+        @name == 'Beta'
+    end
+end
+
+def largest_company
+
+end
+
+def companies_from_largest_to_smallest
+
+end
+binding.pry
