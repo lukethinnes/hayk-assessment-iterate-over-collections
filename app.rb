6fa803142d9f42ae7a155147420b61926cf91d2f
Luke Thinnes <lukethinnes@yahoo.com>
2020-12-16 15:47:14 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -14,3 +16,41 @@ companies = [
 ]
 
 ### Your code below this
+
+def name_and_size(companies)
+    companies.map do |company|
+        puts "#{company.name} - #{company.size}"
+    end
+end
+
+def total_size(companies)
+    companies.reduce(0) do |total, company|
+        total + company.size
+    end
+end	
+
+def companies_over_100(companies)
+    companies.filter do |company| 
+        if company.size > 100
+        puts company.name
+        end
+    end
+end
+
+def company_named_beta(companies)
+    companies.find do |company| 
+        company.name == "Beta"
+    end
+end
+
+def largest_company(companies)
+    companies.max_by do |company| 
+        company.size
+    end
+end
+
+def sort(companies)
+    companies.reverse do |company| 
+        company.size
+    end 	
+end
